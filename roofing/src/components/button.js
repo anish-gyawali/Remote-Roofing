f1b9337dc1710123fd0a44d226faf26e44864f9a
Anish <anish.gyawali@selu.edu>
2020-11-04 08:45:47 -0600

    added header
    Removed unused components

---
+++ roofing/src/components/button.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './components.css';
+
+const Button =({title})=>{
+    return(
+<div className="button">{title}</div>
+)
+}
+export default Button;
---
+++ roofing/src/section/header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="container">
+        <div>      
+            <div className="header-first">
+        <a href="http://remoteroofing.com/">
+          <div>
+            <img src={require("../assets/logo.png")} />
+          </div>
+        </a>
+        <div>
+          <img src={require("../assets/phone.png")} />
+        </div>
+        <div>
+          <a href="tel:+1-800-401-7716">
+            <div>
+              <strong>+1 (800) 401-7716</strong>
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <div className='header-second'>
+        <div className='home'>Home</div>
+        <div className='services'>Services</div>
+        <div className='works'>How it works</div>
+      </div>
+      <div className='header-body'>
+    <div>
+        <h1> Free roof Repair & local Roofing Quote</h1>
+        <h2>Get your roof inspected remotely and connect with our trusted roofers</h2>
+    </div>
+    <div>
+        {/* form will be added with botton */}
+    </div>
+      </div>
+      </div>
+
+    </div>
+  );
+}
+export default Header;
